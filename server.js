0f6b3b209f1a53a85bdaec38cbb97dcd13123ca8
20058967 <fauzan.rachmatullah@btpn.com>
2024-02-28 08:39:39 +0700

    add dependecy body parser

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import router from './routers/employeeRouter.js';
+import cors from 'cors'
+import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
-app.use(express.json());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => {
